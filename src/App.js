b7505676bace01d4550b0f28ecce181b4128dd9a
prediatorr <rajeevkumarsingh5252@gmail.com>
2023-06-25 12:35:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./index.scss";
 function App() {
+  const [data, setData] = useState(null);
+  const [search, setSearch] = useState("");
+  function searching(e) {
+    setSearch(e.target.value);
+  }
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
+      .then((res) => res.json())
+      .then((d) => {
+        setData(d);
+        console.log("heyaa");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <input type="text" placeholder="Search..." onChange={searching} />
+      {data &&
+        data.data
+          .filter((item) => {
+            return item.first_name.toLowerCase().includes(search);
+          })
+          .map((item, index) => {
+            return (
+              <div className="item" key={item.id}>
+                <div className="item-id">{item.id}</div>
+                <div className="box">
+                  <div className="images">
+                    <img src={item.avatar} alt="" />
+                  </div>
+                </div>
+                <div className="item-name">{item.first_name}</div>
+              </div>
+            );
+          })}
     </div>
   );
 }
